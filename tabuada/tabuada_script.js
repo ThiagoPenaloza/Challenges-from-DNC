566e3af890c3861aff825381b87c5e8e62d2cbdb
Thiago Peñaloza <thiagoalmeidapenaloza@gmail.com>
2023-03-21 16:32:20 -0300

    last commit

---
+++ tabuada/tabuada_script.js
@@ -0,0 +1,19 @@
+function tabuada() {
+    var num = window.document.getElementById(`num`)
+    var end = document.getElementById(`end`)
+    var n = Number(num.value)
+    var e = Number(end.value)
+    var x = 0
+    var r = ''
+    var res = window.document.getElementById(`resposta`)
+    if ( n == 0 || e == 0) {
+        window.alert('error')
+    } else {
+    res.innerHTML = `A Tabuada do ${n} é: <br>`
+    do{
+        r = n*x
+        res.innerHTML += `${x} X ${n} = ${r} <br>`
+        x++
+    } while (x <= e)
+    }
+}
